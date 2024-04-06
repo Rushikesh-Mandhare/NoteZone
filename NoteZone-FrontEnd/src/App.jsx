dfebdb761e115074410610a13f8cd8a6f06b2d96
Rushikesh-Mandhare <rushikesh.mandhare@mitaoe.ac.in>
2024-04-06 19:41:17 +0530

    initial commit

--- NoteZone-FrontEnd/src/App.jsx
+++ NoteZone-FrontEnd/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
 import Home from "../Components/Home";
 import Navbar from "../Components/Navbar";
@@ -6,19 +6,33 @@ import About from "../Components/About";
 import NotesState from '../context/NotesState';
 import Login from '../Components/Login';
 import Signup from '../Components/Signup';
+import AlertComponent from '../Components/Alerts'; // Rename Alert to avoid conflict
 
 function App() {
+  const [alertState, setAlertState] = useState(null); // Rename Alert to avoid conflict
+
+  const showAlert = (message, type) => {
+    setAlertState({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlertState(null);
+    }, 1500);
+  };
+
   return (
     <NotesState>
       <Router>
         <Navbar />
+        <AlertComponent alert={alertState} /> {/* Pass alert prop */}
         <div className="container">
-        <Routes> {/* Wrap Routes with a Routes element */}
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home showAlert={showAlert} />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Login" element={<Login showAlert={showAlert} />} />
+            <Route path="/Signup" element={<Signup showAlert={showAlert} />} />
+          </Routes>
         </div>
       </Router>
     </NotesState>
