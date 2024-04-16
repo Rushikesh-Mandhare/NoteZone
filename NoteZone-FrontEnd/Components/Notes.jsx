c9b3bb6317c316601ba45939bcf720bdf0158dfc
Rushikesh-Mandhare <rushikesh.mandhare@mitaoe.ac.in>
2024-04-17 00:57:49 +0530

    Revert "initial commit"
    This reverts commit 92364f8264b9857105d926bc6fbb6838494b34be.

--- NoteZone-FrontEnd/Components/Notes.jsx
+++ NoteZone-FrontEnd/Components/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from 'react';
+import React , {useContext, useEffect} from 'react';
 import NotesContext from '../context/NotesContext';
 import Notesitem from './Notesitem';
 import AddNote from "./Addnotes.jsx";
@@ -35,7 +35,7 @@ const Notes = () => {
         console.log("Updating the Note", note)
         editNote(note.id, note.title, note.description, note.tag )
         refClose.current.click()
-        event.preventDefault(); // Prevent default form submission behavior
+        // event.preventDefault(); // Prevent default form submission behavior
         // add(note.title, note.description, note.tag);
         // setNote({ title: "", description: "", tag: "" }); // Clear input fields after submission
         // setShowModal(false); // Close the modal after saving changes
@@ -45,56 +45,20 @@ const Notes = () => {
         setNote({ ...note, [e.target.name]: e.target.value });
     };
 
-    return (
-        <>
-            <div>
-                <AddNote />
-            </div>
-
-            <div>
-                <button type="button" className="btn btn-primary d-none" onClick={updateNote} ref={modalRef}>
-                    Launch demo modal
-                </button>
-                <div className={`modal fade ${showModal ? 'show' : ''}`} id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden={!showModal} style={{ display: showModal ? 'block' : 'none' }}>
-                    <div className="modal-dialog" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
-                            </div>
-                            <div className="modal-body">
-                                <form className="my-4" onSubmit={handleSubmit}>
-                                    <div className="form-group mb-3">
-                                        <label htmlFor="title"><h4>Title</h4></label>
-                                        <input type="text" className="form-control" id="title" name="title" placeholder="Enter Title" value={note.title} onChange={onChange} />
-                                    </div>
-                                    <div className="form-group mb-3">
-                                        <label htmlFor="description"><h4>Description</h4></label>
-                                        <input type="text" className="form-control" id="description" name="description" placeholder="Description" value={note.description} onChange={onChange} />
-                                    </div>
-
-                                    <div className="form-group mb-3">
-                                        <label htmlFor="tag"><h4>Tag</h4></label>
-                                        <input type="text" className="form-control" id="tag" name="tag" placeholder="Tag" value={note.tag} onChange={onChange} />
-                                    </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" onClick={closeModal}>Close</button>
-                                        <button type="submit" className="btn btn-primary" ref={refClose} >Save changes</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="row my-3">
-                <h3>Your Notes</h3>
-                {notes.map((note) => {
-                    return <Notesitem key={note._id} openModal={updateNote} deleteNote={deleteNote} note={note} />;
-                })}
-            </div>
-        </>
-    );
+  return (
+    <>
+    <div>
+        <AddNote/>
+      </div>
+    
+    <div className="row my-3">
+      <h3>Your Notes</h3>
+      {notes.map((notes)=>{
+        return <Notesitem key={notes._id} note= {notes}/>
+      })}
+      </div>
+      </>
+  )
 }
 
-export default Notes;
+export default Notes
--- NoteZone-FrontEnd/Components/Notesitem.jsx
+++ NoteZone-FrontEnd/Components/Notesitem.jsx
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import NotesContext from '../context/NotesContext';
 
-const Notesitem = ({ note, openModal, deleteNote }) => {
+const Notesitem = (props) => {
+
+    const context = useContext(NotesContext);
+    const { deleteNote } = context;
+
+    const { note } = props
     return (
         <div className="col-md-3">
-            <div className="card my-3">
-                <div className="card-body d-flex justify-content-between align-items-center">
-                    <div>
-                        <h5 className="card-title">{note.title}</h5>
-                        <p className="card-text">{note.description}</p>
-                    </div>
-                    <div className="d-flex align-items-center">
-                        <lord-icon
-                            src="https://cdn.lordicon.com/zfzufhzk.json"
-                            trigger="hover"
-                            delay="1500"
-                            stroke="bold"
-                            state="hover-line"
-                            style={{ width: "40px", height: "60px", marginRight: "10px", cursor: "pointer" }}
-                            onClick={() => { openModal(note) }}
-                        >
-                        </lord-icon>
-                        <lord-icon
-                            src="https://cdn.lordicon.com/xekbkxul.json"
-                            trigger="hover"
-                            stroke="bold"
-                            style={{ width: "40px", height: "60px", cursor: "pointer" }}
-                            onClick={() => { deleteNote(note._id) }}
-                        >
-                        </lord-icon>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  <div className="card my-3">
+    <div className="card-body d-flex justify-content-between align-items-center">
+      <div>
+        <h5 className="card-title">{note.title}</h5>
+        <p className="card-text">{note.description}</p>
+      </div>
+      <div className="d-flex align-items-center ">
+        <lord-icon
+          src="https://cdn.lordicon.com/zfzufhzk.json"
+          trigger="hover"
+          delay="1500"
+          stroke="bold"
+          state="hover-line"
+          style={{ width: "40px", height: "60px" , marginRight: "10px" ,cursor:"pointer"}}
+        >
+        </lord-icon>
+        <lord-icon
+          src="https://cdn.lordicon.com/xekbkxul.json"
+          trigger="hover"
+          stroke="bold"
+          style={{ width: "40px", height: "60px" ,cursor:"pointer"}}
+          onClick={() => { deleteNote(note._id) }}
+        >
+        </lord-icon>
+      </div>
+    </div>
+  </div>
+</div>
+
+    )
 }
 
-export default Notesitem;
+export default Notesitem
