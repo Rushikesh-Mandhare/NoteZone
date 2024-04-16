e256228f28ac13bbcbde97661932b84ae6df4db4
Rushikesh-Mandhare <rushikesh.mandhare@mitaoe.ac.in>
2024-04-17 00:59:23 +0530

    Revert "initial commit"
    This reverts commit 92364f8264b9857105d926bc6fbb6838494b34be.

--- NoteZone-FrontEnd/Components/Notes.jsx
+++ NoteZone-FrontEnd/Components/Notes.jsx
@@ -4,46 +4,16 @@ import Notesitem from './Notesitem';
 import AddNote from "./Addnotes.jsx";
 
 const Notes = () => {
-    const context = useContext(NotesContext);
-    const { notes, add, getNotes, editNote, deleteNote } = context;
 
-    useEffect(() => {
-        getNotes();
-    }, []);
-
-    const modalRef = useRef(null);
-    const [showModal, setShowModal] = useState(false);
-    const [note, setNote] = useState({ id:"", title: "", description: "", tag: "" }); // State for form inputs
-
-    const updateNote = (currentNote) => {
-        setShowModal(true);
-        setNote({
-            id:currentNote._id,
-            title: currentNote.title,
-            description: currentNote.description,
-            tag: currentNote.tag
-        });
-    };
-
-    const closeModal = () => {
-        setShowModal(false);
-    };
-
-    const refClose= useRef(null)
-
-    const handleSubmit = (event) => {
-        console.log("Updating the Note", note)
-        editNote(note.id, note.title, note.description, note.tag )
-        refClose.current.click()
-        // event.preventDefault(); // Prevent default form submission behavior
-        // add(note.title, note.description, note.tag);
-        // setNote({ title: "", description: "", tag: "" }); // Clear input fields after submission
-        // setShowModal(false); // Close the modal after saving changes
-    };
-
-    const onChange = (e) => {
-        setNote({ ...note, [e.target.name]: e.target.value });
-    };
+  const context = useContext(NotesContext);
+  const {notes, add, getNotes} =context;
+  
+  useEffect(() => {
+    
+      getNotes()
+    
+  }, [])
+  
 
   return (
     <>
